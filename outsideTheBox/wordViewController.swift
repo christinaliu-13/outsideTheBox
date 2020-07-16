e3d180f31dde40129c303cefe5deffc12778f6f5
Christina Liu <christinaflorida@gmail.com>
2020-07-16 09:48:32 -0700

    some word of the day trial

--- outsideTheBox/wordViewController.swift
+++ outsideTheBox/wordViewController.swift
@@ -10,21 +10,26 @@ import UIKit
 
 class wordViewController: UIViewController {
 
+    @IBOutlet weak var wordLabel: UILabel!
+    @IBOutlet weak var definitionLabel: UILabel!
+    @IBOutlet weak var sentenceLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func random(){
+        let word = ["agender", "gay"]
+        let definitions = ["Someone who identifies as not having any gender, regardless of physical appearances or traits.", "orange"]
+        let sentence = ["In high school, Kate identified as 'agender' and used the singular pronoun 'they' instead of he or she.", "She is a gay orange."]
+        let index = Int.random(in: 1...3)
+        wordLabel.text = word[index]
+        definitionLabel.text = definitions[index]
+        sentenceLabel.text = sentence[index]
     }
-    */
 
-}
+    
+
+}//end of class
