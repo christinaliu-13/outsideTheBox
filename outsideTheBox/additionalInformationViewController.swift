13e457f809c24e98fd5787b7f5506c205b78820f
Stella Hong <stellahong0128@gmail.com>
2020-07-16 11:22:05 -0700

    dictionary

---
+++ outsideTheBox/additionalInformationViewController.swift
@@ -0,0 +1,30 @@
+//
+//  wordsViewController.swift
+//  outsideTheBox
+//
+//  Created by Stella Hong on 7/16/20.
+//  Copyright © 2020 Christina Liu. All rights reserved.
+//
+
+import UIKit
+
+class wordsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ outsideTheBox/wordInformationViewController.swift
@@ -0,0 +1,30 @@
+//
+//  wordInformationViewController.swift
+//  outsideTheBox
+//
+//  Created by Stella Hong on 7/16/20.
+//  Copyright © 2020 Christina Liu. All rights reserved.
+//
+
+import UIKit
+
+class wordInformationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
